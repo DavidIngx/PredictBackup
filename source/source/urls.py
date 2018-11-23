af0d51388ad97eb1b6e4affebb81551b2bf9da1a
DavidIngX <brian.penam@campusucc.edu.co>
2018-11-23 00:01:04 -0500

    advance load of connected devicecs in the machine

--- source/source/urls.py
+++ source/source/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('load_dataset/', views.simple_upload),
     path("read_file/", views.read_file),
+    path("read_disk/", views.read_disk),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
--- source/source/views.py
+++ source/source/views.py
@@ -3,17 +3,25 @@ from django.shortcuts import loader, render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import pandas as pd
+import os
 
 def simple_upload(request):
+    os.chdir("/media/linux")
+    drives = os.popen("ls","r")
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+
+
+
         return render(request, 'load_dataset.html', {
             'uploaded_file_url': uploaded_file_url
+
         })
-    return render(request, 'load_dataset.html')
+    return render(request, 'load_dataset.html',{
+    'drives':drives,})
 
 
 def read_file(request):
@@ -23,6 +31,9 @@ def read_file(request):
     'make_list': prueba
 })
 
+def read_disk(request):
+    p = os.popen("tree /home/linux/PredictBackup/ -D  -f -s -i","r")
+    return render(request, 'showfile.html', {'make_list': p})
 
 def prueba(response):
     pruebax = loader.get_template("simple_upload.html")
